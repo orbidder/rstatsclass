78a33f5b07460a0feaf17845af2912c2a16b5311
Owen <o.r.bidder@gmail.com>
2019-08-07 17:03:55 -0700

    Worked up until rasters, added a DEM for the wiggins area.

--- Seminar 2 Spatial Data in R/Seminar Spatial.R
+++ Seminar 2 Spatial Data in R/Seminar Spatial.R
@@ -9,6 +9,10 @@ library(rgdal)
 library(raster)
 library(adehabitatLT)
 library(rgeos)
+library(sf)
+library(mapview)
+library(ggspatial)
+library(plyr)
 
 #R has a rich ecosystem of packages to perform spatial analysis. In this session, we will run quickly over
 #the different spatial data types, how they are handled in R, and some simple functions that we use
@@ -76,10 +80,13 @@ plot(elk_utm, axes = T) #Notice now axes show metres!
 #Lines#
 #lines are similar to points, but now each feature is represented by a vector of coordinates (c(x,x,x),c(y,y,y))
 #common examples we might encounter in movement ecology are roads and rivers.
-#Lines can be loaded and set up manually as above, but given a lot of this information will be provided to you
-#in ESRI shapefiles, we will load an example using the readOGR function in rgdal
 
-rivers <- readOGR('wiggins_rivers.shp')
+#Lines can be loaded and set up manually as above with sp, but given a lot of this information will be provided to you
+#in ESRI shapefiles, we will load an example using the readOGR function in rgdal. You should note that a
+#lot of spatial reading and writing is done using rgdal, which is the r wrapper package for the GDAL
+#library.
+
+rivers <- readOGR('wiggins_rivers.shp') #note this creates the SpatialLinesDataFrame, an sp class object
 proj4string(rivers)
 
 #We can access the attribute table for this layer by looking in the data slot
@@ -101,58 +108,188 @@ proj4string(pub_lands) #check the projection
 proj4string(elk_utm) == proj4string(pub_lands)
 
 plot(pub_lands)
-plot(pub_lands, axes = T, lwd = 2) ; plot(rivers, col = 'blue', axes=T, add=T) #plot our elk data with the lands
+plot(pub_lands, axes = T, lwd = 2) ; plot(rivers, col = 'blue', axes=T, add=T) #plot our rivers and public lands
 
 #Common spatial functions#
+#So far we've seen how rgdal helps get spatial data in to R and sp provides methods and classes to handle 
+#the data once its there...
 
-#When modelling animal movement, you might want to calculate displacement distances and turn angles
-#between successive relocations. This can be done quickly using as.ltraj from adehabitatLT
-
-elk_ltraj <- as.ltraj(xy = data.frame(elk_utm$Longitude,elk_utm$Latitude),
-                       id = elk_utm$Elk,
-                       date = elk_utm$Date_Time, burst = elk_utm$id, 
-                       infolocs = data.frame(pkey = paste(elk_utm$id, elk_utm$Date_Time, sep = "."), 
-                                             row.names = row.names(data.frame(elk_utm$Longitude,elk_utm$Latitude))),
-                       proj4string = CRS("+proj=utm +zone=12 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-elk_df <- ld(elk_ltraj) #creates a dataframe from an ltraj object
-
-colnames(elk_df) #column "dist" holds the displacement distances and "rel.angle" the turn angles in radians
+#A third package, rgeos, provides some basic functions to manipulate the data. These three packages
+#form the core of the sp "R-Spatial" package ecosystem.
 
 #The rgeos package contains a number of useful spatial functions for dealing with sp objects...
+
 #gDistance measures the distance between two features
 
-gDistance(elk_utm[1,], pub_lands[pub_lands$SURFACE == "State",]) #finds the nearest parcel of state land to first location
+gDistance(elk_utm[1,], pub_lands[pub_lands$SURFACE == "State",]) #finds distance between point #1 and nearest state land
 
 #gBuffer makes a buffer around an sp object, output is a polygon
-elk_500m <- gBuffer(elk_utm[1:5,], width = 500)
-plot(elk_500m); plot(elk_utm[1:5,], col = 'blue', add = T)
+elk_500m <- gBuffer(elk_utm[1:5,], width = 500) #just do the first 5 points because it's quick
+plot(elk_500m, lwd = 2, col = 'green'); plot(elk_utm[1:5,], col = 'blue', add = T)
 
 #gIntersects returns true or false whether two features touch, which is useful for checking if elk are
 #on public land
 gIntersects(elk_utm[1,], pub_lands, byid = T) #include argument byid=T to check each polygon
 pub_lands$SURFACE[gIntersects(elk_utm[1,], pub_lands, byid = T)] #returns the value of SURFACE at elk_utm[1,]
 
-#Another common task is to measure the distance of each GPS location to a linear feature like a river, this can be
-#done using the gDistance function in rgeos...
+#Exercise 2:
+#A common task is to measure the distance of each GPS location to a linear feature like a river. Running
+#gDistance(elk_utm, rivers, byid=T) would return an nrow(x) by nrow(y) matrix, and we aren't too bothered
+#about comparing each location to each point on every river (takes a long time on large layers!). 
 
+#Write a for loop that fills a new column, 'dist_river', in the elk_utm attribute table, 
+#with the distance between each location and the nearest point on the rivers object (10 minutes)
+
+#I've included a template for loop for you to fill in below, which will monitor progress using a
+#progress bar, and also time the calculations for us to compare our solutions...
+
+#For Loop Template#
 dechrau <- Sys.time()
+total <- #enter an appropriate max number here
 pb <- txtProgressBar(min = 0, max = total, style = 3)
-for (i in 1:length(elk_utm)) {
-  elk_utm$dist_river[i] <- gDistance(elk_utm[i,], rivers)
+for (i in 1:total) {
+ 
+  #Enter your code here#
+  
   setTxtProgressBar(pb, i)
 }
 close(pb)
 diwedd <- Sys.time()
-period <- round(diwedd-dechrau,2)
-print(paste("Distance measurement took",round(diwedd-dechrau,2),attributes(period)$units,sep = " "))
+amser <- diwedd-dechrau
+print(paste("Measurements took",round(diwedd-dechrau,2),attributes(amser)$units,sep = " "))
 
+#########################################################################################################
 
+#Answer #2:
+dechrau <- Sys.time()
+total <- length(elk_utm )
+pb <- txtProgressBar(min = 0, max = total, style = 3)
+for (i in 1:total) {
+  
+  #some code to speed up calcs when dealing with very big lines and polygons!
+  #buffer <- gBuffer(elk_utm[i,], width = 10000)
+  #plot(buffer)
+  #cut_river <- rivers[c(gIntersects(rivers, buffer, byid=T)),]
+  #plot(buffer);plot(rivers, add=T)
+  #plot(buffer);plot(cut_river, add=T)
+  
+  elk_utm$dist_river[i] <- gDistance(rivers, elk_utm[i,])
+  setTxtProgressBar(pb, i)
+}
+close(pb)
+diwedd <- Sys.time()
+amser <- diwedd-dechrau
+print(paste("Distance measurement took",round(diwedd-dechrau,2),attributes(amser)$units,sep = " "))
+
+head(elk_utm$dist_river)
 
 #SF#
-#Mixed Objects#
+#As mentioned above, the sf package has many of sp's functionality, but has a couple of important advantages,
+#namely, that it is faster, works with tidyverse packages, and can combine different geometry types in 
+#the same object
+
+#reading in csv files with sf is similar to sp
+elk_data <- read.csv("Spatial_Wiggins_Elk.csv", header = T, stringsAsFactors = F)
+
+elk_data <- st_as_sf(elk_data, 
+                     coords = c('Longitude', 'Latitude'), 
+                     crs = '+init=epsg:4326')
+
+#Converting an sp object to an sf object is easy!
+elk_sf <- st_as_sf(elk_utm)
+
+#As is loading in ESRI Shapefiles
+rivers_sf <- st_read('wiggins_rivers.shp')
+pub_lands_sf <- st_read('wiggins_lands.shp')
+
+
+#Plotting with sf shows a summary for each layer
+plot(elk_sf)
+
+#...and we can plot specific layers by name
+plot(elk_sf['dist_river'])
+
+#and use ggplot2 to make nice maps quite quickly...
+ggplot() + geom_sf(data = elk_sf, aes(color = dist_river)) + 
+  geom_sf(data = rivers_sf) +
+  annotation_scale(location = "br", width_hint = 0.5) +
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         style = north_arrow_fancy_orienteering) +
+  theme_light()
+
+#...and even fancier interactive plots with mapview!
+map <- mapview(pub_lands_sf['SURFACE'],
+               layer.name = 'Land Ownership')
+map
+
+#note: you can add multiple layers to mapview by entering them as a list(), and including a vector of layer names
+
+#and of course sf has some useful spatial functions. For instance joins can take attributes from one
+#object and add them to another.
+
+pub_lands_sf$id <- as.factor(1:312)
+elk_sf <- st_join(elk_sf, pub_lands_sf, join = st_intersects, left = T) #adds all pub_lands_sf attributes
+plot(elk_sf['SURFACE'])
+
+#and we can make plots of how many points are in each polygon
+point_in_poly <- st_join(elk_sf, pub_lands_sf, join = st_within)
+
+point_poly_count <- count(point_in_poly$id)
+
+tract_point_sf <- left_join(pub_lands_sf, point_poly_count, by = c("id" = "x"))
+plot(tract_point_sf['freq'])
+
+#sf also has its own distance function, st_distance(), you may see some tutorials with the following method
+#Dont run...
+#dist <- st_distance(y=elk_sf, x=rivers_sf) # a matrix of all pair-wise distances (all pnts to all lines)
+#elk_sf$dist_river_sf <- apply(dist, 2, min) #run through, finding the minimum for each one
+
+#...but I find the for loop much faster. Let's compare with the earlier sp version by timing it, and we'll
+#make a new column so we can check the values between the methods
+dechrau <- Sys.time() #start the timer
+for (i in 1:length(elk_sf)) {
+  elk_sf$dist_river_sf[i] <- min(st_distance(y=elk_sf[i,], x=rivers_sf, by_element = T))
+}
+diwedd <- Sys.time() #stop the timer
+amser <- diwedd-dechrau #calc the time elapsed
+print(paste("Measurements took",round(diwedd-dechrau,2),attributes(amser)$units,sep = " "))
+
+head(elk_sf) #values are the same, but its much faster!
 
 #Rasters#
-#Loading rasters, raster stacks#
-#Plotting#
-#Extracting values#
-#Raster calculations#
+#So far we have concentrated on vector data; points, lines and polygons. A fourth and very important data
+#type is the raster. These are essentially 2D matrices that represent a grid of the earth's surface. For
+#satellite imagery, each cell represents a pixel from the image. A common use of the raster datatype is
+#elevation data...
+
+#Loading Digital Elevation Model (DEM) for our area...
+elev <- raster('wiggins_dem.tif')
+plot(elev) ; plot(elk_sf[2], add=T) #plot elevation and overlay the elk locations
+
+#we can get some info about are raster easily...
+extent(elev)
+res(elev)
+crs(elev)
+elev #calling elev will give us a lot of summary info at once
+
+#The raster package is one of the most comprehensive packages available for R. The documentation is excellent
+#and I encourage you to read it to see the breadth of things you can do.
+
+#For instance, raster contains useful terrain functions for calculating aspect, slope etc.
+asp <- terrain(elev, opt = "aspect")
+slo <- terrain(elev, opt = "slope")
+
+plot(asp)
+plot(slo)
+
+#These different layers can be combined in to a 'Raster Stack'
+dem <- stack(elev,asp,slo)
+plot(dem)
+
+#think of a stack as a 3D matrix, where each layer or slice represents a new dimension of the same
+#area in space (in our example, elevation, aspect and slope). When measuring things like NDVI, we might
+#stack revisits by the satellite and index them by time, allowing us to look at trends over the seasons
+#(i.e. phenology) or to look at conditions on the ground in time and space for use in our movement
+#models (see session on SSFs)
+
+#Some restrictions on stacks are; they must have the same extent, resolution and projection.
